21e91d15c2c95172cf351257c837bff56bc26d82
Steven Howell <showell@us.ibm.com>
2017-09-18 08:50:12 -0400

    initial server setup

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 /* Hey! This file runs a server. When people connect to our web site, this helps them find the game they are looking for. Have questions about what's happening here? Ask Steven! */
+const express = require('express'),
+    config = require('./package'),
+    server = express();
+
+server.use(express.static('public'));
+
+server.get('/', (req, res) => {
+    res.send("Hello world!");
+});
+
+server.listen(config.port, () => {
+  console.log(`Server listening on port ${config.port}`);
+});
